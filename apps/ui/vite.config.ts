84b2b6fee6aee386e313a4e52aa1a33f3b1dea06
Alexis Tacnet <alexistacnet@gmail.com>
2025-04-27 16:01:29 +0200

    chore: clean vite config for UI

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -4,11 +4,9 @@ import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
-import type { ManifestEntry } from "workbox-build";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     build: {
       sourcemap: true,
@@ -18,98 +16,57 @@ export default defineConfig(({ mode }) => {
       VitePWA({
         registerType: "autoUpdate",
         injectRegister: "inline",
-        scope: "https://banqr.app/",
-        workbox: {
-          globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
-          ignoreURLParametersMatching: [/^v$/],
-          manifestTransforms: [
-            (manifestEntries: (ManifestEntry & { size: number })[]) => {
-              manifestEntries.forEach((entry) => {
-                entry.url = `https://client.banqr.app/${entry.url}`;
-              });
-              return { manifest: manifestEntries };
-            },
-          ],
-          inlineWorkboxRuntime: true,
-          navigateFallback: null,
-          runtimeCaching: [
-            {
-              urlPattern: /^https:\/\/banqr\.app\/.*/i,
-              handler: "NetworkFirst",
-              options: {
-                precacheFallback: {
-                  fallbackURL: "https://client.banqr.app/index.html",
-                },
-              },
-            },
-            {
-              urlPattern: /^https:\/\/client\.banqr\.app\/.*/i,
-              handler: "CacheFirst",
-              options: {
-                cacheName: "banqr-client-ressources",
-                expiration: {
-                  maxEntries: 10,
-                  maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-                },
-                cacheableResponse: {
-                  statuses: [0, 200],
-                },
-              },
-            },
-          ],
-        },
-        includeAssets: ["https://client.banqr.app/favicon.ico"],
+        includeAssets: ["favicon.ico"],
         manifest: {
           name: "Maille",
           short_name: "Maille",
           description: "Maille is a personal finance for developers.",
-          theme_color: "#ffffff",
-          start_url: "https://banqr.app/login",
+          theme_color: "#1C1C1C",
           icons: [
             {
-              src: "https://client.banqr.app/icon-72x72.png",
+              src: "icon-72x72.png",
               sizes: "72x72",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-96x96.png",
+              src: "icon-96x96.png",
               sizes: "96x96",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-128x128.png",
+              src: "icon-128x128.png",
               sizes: "128x128",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-144x144.png",
+              src: "icon-144x144.png",
               sizes: "144x144",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-152x152.png",
+              src: "icon-152x152.png",
               sizes: "152x152",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-192x192.png",
+              src: "icon-192x192.png",
               sizes: "192x192",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-384x384.png",
+              src: "icon-384x384.png",
               sizes: "384x384",
               type: "image/png",
               purpose: "maskable any",
             },
             {
-              src: "https://client.banqr.app/icon-512x512.png",
+              src: "icon-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "maskable any",
@@ -117,32 +74,10 @@ export default defineConfig(({ mode }) => {
           ],
         },
       }),
-      sentryVitePlugin({
-        org: "fuegoio",
-        project: "banqr",
-        telemetry: false,
-        disable: mode !== "production",
-      }),
     ],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    experimental: {
-      renderBuiltUrl(
-        filename: string,
-        {
-          hostId,
-          hostType,
-          type,
-        }: {
-          hostId: string;
-          hostType: "js" | "css" | "html";
-          type: "public" | "asset";
-        }
-      ) {
-        return "https://client.banqr.app/" + filename;
       },
     },
   };
