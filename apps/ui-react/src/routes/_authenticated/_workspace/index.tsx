52bb193b030cb2d67760803cd4a723c144aa3026
Alexis Tacnet <alexis@mistral.ai>
2026-02-23 18:44:36 +0100

    update dashboard

--- apps/ui-react/src/routes/_authenticated/_workspace/index.tsx
+++ apps/ui-react/src/routes/_authenticated/_workspace/index.tsx
@@ -1,20 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+
 export const Route = createFileRoute("/_authenticated/_workspace/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
-    </>
+    <SidebarInset>
+      <header className="flex h-12 shrink-0 items-center gap-2 border-b pr-2 pl-4">
+        <SidebarTrigger className="mr-1" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+    </SidebarInset>
   );
 }
