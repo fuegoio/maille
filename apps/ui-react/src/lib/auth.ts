d7d5e68188e2732ab0e939577dfab6e43a0954ae
Alexis Tacnet <alexis@mistral.ai>
2026-02-23 11:27:52 +0100

    update auth

--- apps/ui-react/src/lib/auth.ts
+++ apps/ui-react/src/lib/auth.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
+const baseURL = import.meta.env.VITE_API_URL.startsWith("http")
+  ? import.meta.env.VITE_API_URL + "/auth"
+  : window.location.origin + import.meta.env.VITE_API_URL + "/auth";
+
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL,
 });
