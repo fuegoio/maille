55af4858c75537d243469f3d72bdd6b982364062
Alexis Tacnet <alexis@mistral.ai>
2026-02-26 15:13:35 +0100

    add pwa

--- apps/ui-react/vite.config.ts
+++ apps/ui-react/vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import graphqlCodegen from "vite-plugin-graphql-codegen";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,53 @@ export default defineConfig({
     react(),
     tailwindcss(),
     graphqlCodegen(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      injectRegister: "inline",
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Maille",
+        short_name: "Maille",
+        id: "app.fuegoio.maille",
+        description: "Maille is a personal finance management app.",
+        categories: ["finance", "productivity"],
+        orientation: "portrait",
+        lang: "en",
+        dir: "ltr",
+        display: "standalone",
+        start_url: "/",
+        theme_color: "#16161C",
+        background_color: "#16161C",
+        icons: [
+          {
+            src: "icons/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
