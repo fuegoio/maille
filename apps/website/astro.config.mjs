edddb7d3d1345e3a0f62074591d993f21bc1d24e
Alexis Tacnet <alexis@mistral.ai>
2026-09-15 15:34:03 +0100

    feat(website): build marketing website

---
+++ apps/website/astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// Update `site` when the production domain is known.
+export default defineConfig({
+  site: "https://fuegoio.github.io/maille",
+});
